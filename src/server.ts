24c058d71c6e4a4ebb643592dbce85dfb2fe7ec8
BlockchainRafik <151425470+blockchainrafik@users.noreply.github.com>
2026-09-03 19:08:40 +0100

    Change import path from './app' to './index'

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import http from 'http';
-import app from './app';
+import app from './index';
 import { setupGracefulShutdown } from './gracefulShutdown';
 
 const PORT = Number(process.env.PORT || 3000);
